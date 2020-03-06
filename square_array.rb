74f1261043f1fb652595e4a02a939303378a85a3
Michaylah Mam <mchylh@gmail.com>
2020-03-06 14:18:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  new_array = [1,2,3]
+
+  numbers.each do |number|
+  new_array << number ** 2
+ end
+ return new_array
 end
+new
